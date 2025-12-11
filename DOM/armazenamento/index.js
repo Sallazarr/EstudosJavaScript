4f7852ee8264df588ae7e69fb9a96f7743b1b323
Henrique Salazar da Silva <henriquesalazards16@hotmail.com>
2025-12-11 18:26:26 -0300

    Storage (Session e Local) e Cookies

---
+++ DOM/armazenamento/index.js
@@ -0,0 +1,41 @@
+document.getElementById('sessionBtn').addEventListener('click', function(){
+    const input = document.getElementById('session')
+    sessionStorage.setItem('info', input.value)
+    input.value = ''
+})
+
+document.getElementById('readSession').addEventListener('click', function(){
+    const info = sessionStorage.getItem('info')
+    alert('Session Storage: ' + info)
+})
+
+document.getElementById('localBtn').addEventListener('click', function(){
+    const input = document.getElementById('local')
+    localStorage.setItem('text', input.value)
+    input.value = ''
+})
+
+document.getElementById('readLocal').addEventListener('click', function(){
+    const text = localStorage.getItem('text')
+    alert('Local Storage: ' + text)
+})
+
+document.getElementById('cookieBtn').addEventListener('click', function(){
+    const input = document.getElementById('cookie')
+    // cookieName=value; expires=UTCStringDate; path=/;
+    const cookie = 'info=' + input.value + ';'
+    const expiration = 'expires=' + new Date(2025, 12, 12) + ';'
+    const path = 'path=/;'
+    document.cookie = cookie + expiration + path
+    input.value = ''
+})
+
+document.getElementById('cookie2Btn'). addEventListener('click', function(){
+    const input = document.getElementById('cookie2')
+    // cookieName=value; expires=UTCStringDate; path=/;
+    const cookie = 'text=' + input.value + ';'
+    const expiration = 'expires=' + new Date(2025, 11, 12) + ';'
+    const path = 'path=/;'
+    document.cookie = cookie + expiration + path
+    input.value = ''
+})
