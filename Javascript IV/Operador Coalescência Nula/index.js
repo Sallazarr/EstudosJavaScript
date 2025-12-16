d3ab4c93b5e30a887c816c62ceee159659ffa2dc
Henrique Salazar da Silva <henriquesalazards16@hotmail.com>
2025-12-16 13:48:52 -0300

    Recursos JS Moderno

---
+++ Javascript IV/Operador Coalescência Nula/index.js
@@ -0,0 +1,25 @@
+// const a = 0
+
+// const b = null
+
+// const c = "Teste"
+
+// console.log(a || b || c)
+
+// console.log( a ?? b ?? c)
+
+
+
+let a = 0
+
+let b = a || 42
+
+console.log( { a, b } )
+
+b = a ?? 4
+
+console.log( { a, b } )
+
+let c = null ?? 42
+
+console.log({c})
