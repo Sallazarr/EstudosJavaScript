2c1a268f7182949f008b509bcd613508c607eeb1
unknown <henriquesalazards16@hotmail.com>
2026-07-06 18:04:51 -0300

    estudos POO

---
+++ Javascript V - POO/04-classes-do-es6/Book.js
@@ -0,0 +1,15 @@
+class Book {
+  constructor(title) {
+    this.title = title;
+    this.published = false;
+  }
+
+  publish() {
+    this.published = true;
+  }
+}
+
+const eragon = new Book("Eragon");
+eragon.publish();
+console.log(eragon);
+console.log(eragon instanceof Book);
